1d4cdaf902ff251a9087db30b9f633892afb4f44
krishnangun <gunavk0018@gmail.com>
2025-11-12 23:09:39 +0530

    removed

--- job-portal-backend/src/main/java/com/jobportal/backend/JobPortalBackendApplication.java
+++ job-portal-backend/src/main/java/com/jobportal/backend/JobPortalBackendApplication.java
@@ -3,7 +3,7 @@ package com.jobportal.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class JobPortalBackendApplication {
 
 	public static void main(String[] args) {
